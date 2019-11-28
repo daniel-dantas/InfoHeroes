a53db910f687c70a378938b5955a37b9860cc03c
XSrDanDanX <ddipaumirim@gmail.com>
2019-11-28 20:54:37 -0300

    Mudando nome do componente Navbar para SearchBar e trabalhando melhor no mesmo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, {Component} from 'react';
-import Navbar from './components/NavBar'
+import React, {Component} from 'react'
+import SearchBar from './components/SearchBar'
 import Card from './components/Card'
 
+
 class App extends Component{
-  constructor(){
-    super()
-    this.state={
-      card: {
-        id: 1,
-        caminhoImagem: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Iron_Man_bleeding_edge.jpg/250px-Iron_Man_bleeding_edge.jpg',
-        nomeHeroi: 'Iron Man'
-      }
-    }
+  state={
+    card: {
+      id: 1,
+      caminhoImagem: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Iron_Man_bleeding_edge.jpg/250px-Iron_Man_bleeding_edge.jpg',
+      nomeHeroi: 'Iron Man',
+    },
+    nomeHeroi: ''
   }
+  
+  getPesquisa = (valorPesquisa) => {
+    this.setState({nomeHeroi: valorPesquisa})
+  }
+
   render(){
     return (
       <div id="app">
-        <Navbar />
+        <SearchBar getPesquisa={this.getPesquisa} pesquisa='pesquisa'/>
         <div className="row">
-          <div className="col s12 m4"></div>
           <div className="col s12 m4">
+          </div>
+          <div className="col s12 m4">
+            
             <Card card={this.state.card}/>
           </div>
           <div className="col s12 m4"></div>
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './style.css'
+
+class SearchBar extends React.Component{
+    
+    constructor(props){
+        super(props)
+        this.state = {
+            pesquisa: props.pesquisa
+        }
+
+    }
+    
+    getPesq = (event) => {
+        this.setState({pesquisa: event.target.value})
+    }
+
+    search = (event) => {
+        this.props.getPesquisa(this.state.pesquisa)
+        event.preventDefault()
+    }
+
+    retornaPesquisa(){
+        return this.state.pesquisa
+    }
+    render(){
+        return (
+            <div id="navbar">
+                 <nav>
+                    <div className="nav-wrapper  blue darken-4">
+                        <form onSubmit={this.search}>
+                            <div className="input-field">
+                                <input id="search"  type="search" onChange={this.getPesq} required placeholder="Digite aqui o nome do heroi"/>
+                                <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
+                                <i className="material-icons">close</i>
+                                <button className="oculto-button" type="submit"></button>
+                            </div>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+
+    
+}
+
+export default SearchBar
